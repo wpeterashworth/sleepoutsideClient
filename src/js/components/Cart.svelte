3cb4af50f6e0fc28863ab66e2815666b2d421d1c
Michael Becker <21beckem8@gmail.com>
2025-10-20 13:48:47 -0600

    cart quantity UI now works

--- src/js/components/Cart.svelte
+++ src/js/components/Cart.svelte
@@ -1,11 +1,20 @@
 <script>
     import CartItem from "./CartItem.svelte";
 
-    const { cartItems, removeItem } = $props();
+    const props = $props();
+    let cartItems = $state(props.cartItems);
+    let removeItem = (item) => {
+        cartItems = cartItems.filter((i) => i.id !== item.id);
+        props.removeItem(item);
+    };
+
 </script>
 
 <ul>
     {#each cartItems as item}
-        <CartItem item={item} removeItem={removeItem} />
+        <CartItem item={item} removeItem={removeItem} cartItems={cartItems} />
     {/each}
+    {#if cartItems.length === 0}
+        <p>Cart is empty. Let;s go <a href="/">shopping</a>!</p>
+    {/if}
 </ul>
--- src/js/components/CartItem.svelte
+++ src/js/components/CartItem.svelte
@@ -1,10 +1,68 @@
 <script>
-    const { item, removeItem } = $props();
-</script>
+  const { item, removeItem } = $props();
 
-<li class="cart-card divider">
-  <button class="cart-card__remove" type="button" title="Remove item" onclick={() => removeItem(item)}>X</button>
-  <a href="/product_pages/index.html?product={item.id}" class="cart-card__image">
+  let quantity = $state(item.quantity);
+
+  function add() { quantity += 1 }
+  function subtract() {
+    quantity -= 1
+    if (quantity < 1) {
+      removeItem(item);
+    }
+  }
+</script>
+<style>
+.card {
+  display: grid;
+  grid-template-columns: 25% auto 15%;
+  font-size: var(--small-font);
+  /* max-height: 120px; */
+  align-items: center;
+  position: relative;
+}
+
+.card__image {
+  grid-row: 1/3;
+  grid-column: 1;
+}
+.card p {
+  margin-top: 0;
+}
+.card .card__name {
+  grid-row: 1;
+  grid-column: 2;
+}
+
+.card__color {
+  grid-row: 2;
+  grid-column: 2;
+}
+.quantity-box {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  width: 200px;
+}
+
+.card__price {
+  grid-row: 2;
+  grid-column: 3;
+}
+button {
+  cursor: pointer;
+  padding: 0.45rem;
+  margin: 0;
+  border-radius: 50%;
+  aspect-ratio: 1;
+  width: 2rem;
+  height: 2rem;
+}
+</style>
+
+<li class="card divider">
+  <a href="/product_pages/index.html?product={item.id}" class="card__image">
     <img
       src="{item.images.primarySmall}"
       alt="{item.name}"
@@ -13,7 +71,14 @@
   <a href="/product_pages/index.html?product={item.id}">
     <h2 class="card__name">{item.name}</h2>
   </a>
-  <p class="cart-card__color">{item.colors[0].colorName}</p>
-  <p class="cart-card__quantity">qty: {item.quantity}</p>
-  <p class="cart-card__price">${item.finalPrice}</p>
+  <p class="card__color">{item.colors[0].colorName}</p>
+  <div style="display: flex; flex-direction: row; align-items: center;">
+    <span>qty: </span>
+    <div class="quantity-box">
+      <button onclick={subtract}>-</button>
+      <span>{quantity}</span>
+      <button onclick={add}>+</button>
+    </div>
+  </div>
+  <p class="card__price">${item.finalPrice}</p>
 </li>
